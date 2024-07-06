06ac5be771e3b0d0a17206d9b48b4587712babbe
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-07-06 14:06:43 +0300

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -1,116 +1,95 @@
 # ---------------------------------------------------------------------------------
 #  /\_/\  🌐 This module was loaded through https://t.me/hikkamods_bot
-# ( o.o )  🔓 Not licensed.
+# ( o.o )  🔐 Licensed under the Copyleft license.
 #  > ^ <   ⚠️ Owner of heta.hikariatama.ru doesn't take any responsibilities or intellectual property rights regarding this script
 # ---------------------------------------------------------------------------------
-# Name: Captcha
-# Author: AleksFolt
+# Name: Verify
+# Author: YourName
 # Commands:
-# .swcaptcha
+# .chatcaptchaon
+# .chatcaptchaoff
 # ---------------------------------------------------------------------------------
 
-# meta developer: @aleksfolt
-
+import asyncio
 import logging
 from typing import List
 
-from telethon import TelegramClient, events, Button, types
-from telethon.tl.functions.channels import EditBannedRequest
-from telethon.tl.types import ChatBannedRights
+import telethon
+from telethon import types
+from telethon.events import ChatAction
+from telethon.tl.functions.channels import EditBannedRequest, EditAdminRequest
+from telethon.tl.types import ChatBannedRights, ChannelParticipantAdmin, ChannelAdminRights
 
 from .. import loader, utils  # type: ignore
 
-logger = logging.getLogger(__name__)
-
 @loader.tds
-class CaptchaMod(loader.Module):
-    """Captcha for chats"""
-
+class VerifyMod(loader.Module):
+    "Verification for new chat members"
+    
     strings = {
-        "name": "Captcha",
-        "pls_pass_captcha": (
-            '<a href="tg://user?id={}">Хэй</a>, пройди капчу! Нажми на кнопку ниже чтобы разблокироваться!'
+        "name": "Verify",
+        "pls_verify": (
+            '<a href="tg://user?id={}">Hello</a>, please verify yourself by clicking the button below within 20 minutes or you will be kicked!'
         ),
-        "captcha_status": "<b>[Captcha]</b> {}",
-        "verification_success": "✅ <b>Вы успешно прошли проверку!</b>",
+        "verify_status": "<b>[Verify]</b> {}",
+        "verify_success": "✅ <a href='tg://user?id={}'>You have been verified!</a>",
+        "verify_fail": "❌ <a href='tg://user?id={}'>You have failed to verify and have been kicked.",
     }
+    
+    class CUserModel(pydantic.BaseModel):
+        chat: int
+        user: int
+        message: int
 
-    class CUserModel:
-        def __init__(self, chat, user, message):
-            self.chat = chat
-            self.user = user
-            self.message = message
-
-    async def client_ready(self, _, db):
+    async def client_ready(self, client, db):
         self.db = db
         self.log = logging.getLogger(__name__)
-        self._db = "CaptchaMod"
+        self._db = "VerifyMod"
         self.locked_users: List[self.CUserModel] = []
-
-    async def watcher(self, m):
+    
+    async def watcher(self, event):
         "Watcher"
-        client: TelegramClient = m.client
-        if isinstance(m, events.ChatAction.Event):
-            if m.chat_id not in self.db.get(self._db, "chats", []):
+        client: telethon.TelegramClient = event.client
+        if isinstance(event, ChatAction.Event):
+            if event.chat_id not in self.db.get(self._db, "chats", []):
                 return
-            if m.user_added or m.user_joined:
-                users = [i.id for i in m.users]
-                for u in users:
-                    _u = await client.get_entity(u)
-                    if _u.bot:
+            if event.user_added or event.user_joined:
+                for user in event.users:
+                    if user.bot:
                         continue
-                    await client.edit_permissions(m.chat_id, u, send_messages=False)
-                    m = await client.send_message(
-                        m.chat_id,
-                        self.strings("pls_pass_captcha").format(u),
-                        buttons=[
-                            Button.inline("Пройти проверку", data=f"verify_{u}")
-                        ],
-                    )
-                    self.locked_users.append(self.CUserModel(m.chat_id, u, m.id))
-                await asyncio.sleep(60)
-                l: List[self.CUserModel] = list(
-                    filter(lambda x: x.chat == m.chat_id and x.user in users, self.locked_users)
-                )
-                if l:
-                    for u in l:
-                        self.locked_users.remove(u)
-                        await client.delete_messages(u.chat, ids=u.message)
-                        await client(
-                            EditBannedRequest(
-                                u.chat,
-                                u.user,
-                                ChatBannedRights(until_date=None, view_messages=True),
-                            )
-                        )
-            elif m.user_kicked or m.user_left:
-                users = [i.id for i in m.users]
-                for u in users:
-                    l: List[self.CUserModel] = list(
-                        filter(lambda x: x.chat == m.chat_id and x.user == u, self.locked_users)
-                    )
-                    if l:
-                        ntt = l[0]
-                        self.locked_users.remove(ntt)
-                        return
+                    self.locked_users.append(self.CUserModel(chat=event.chat_id, user=user.id, message=0))
+                    await client(EditBannedRequest(event.chat_id, user.id, ChatBannedRights(until_date=None, send_messages=True)))
+                    msg = await client.send_message(event.chat_id, self.strings["pls_verify"].format(user.id), buttons=[types.KeyboardButtonCallback("Verify", data=f"verify_{user.id}")])
+                    self.locked_users[-1].message = msg.id
+                
+                await asyncio.sleep(1200)
+                for locked_user in list(filter(lambda x: x.chat == event.chat_id and x.user in [user.id for user in event.users], self.locked_users)):
+                    self.locked_users.remove(locked_user)
+                    await client(EditBannedRequest(locked_user.chat, locked_user.user, ChatBannedRights(until_date=None, view_messages=True)))
+                    await client.kick_participant(locked_user.chat, locked_user.user)
+                    await client.send_message(locked_user.chat, self.strings["verify_fail"].format(locked_user.user))
+        
+        if isinstance(event, types.Message):
+            for locked_user in list(filter(lambda x: x.chat == event.chat_id and x.user == event.sender_id, self.locked_users)):
+                self.locked_users.remove(locked_user)
+                await client(EditBannedRequest(locked_user.chat, locked_user.user, ChatBannedRights(until_date=None, send_messages=None)))
+                await client.delete_messages(locked_user.chat, locked_user.message)
+                await client.send_message(locked_user.chat, self.strings["verify_success"].format(locked_user.user))
 
-        if isinstance(m, events.CallbackQuery.Event):
-            client: TelegramClient = m.client
-            if m.data.startswith(b"verify_"):
-                user_id = int(m.data.split(b"_")[1])
-                if m.query.user_id == user_id:
-                    await client.edit_permissions(m.query.chat_id, user_id, send_messages=True)
-                    await m.answer(self.strings["verification_success"], show_alert=True)
-                    self.locked_users = [user for user in self.locked_users if user.user != user_id]
-                    await client.delete_messages(m.query.chat_id, ids=m.query.msg_id)
-
-    async def swcaptchacmd(self, m: types.Message):
-        "Turn on/off captcha in chat"
+    async def chatcaptchaoncmd(self, m: types.Message):
+        "Turn on captcha in chat"
         l: list = self.db.get(self._db, "chats", [])
         if m.chat_id in l:
-            l.remove(m.chat_id)
-            self.db.set(self._db, "chats", l)
-            return await utils.answer(m, self.strings("captcha_status").format("OFF"))
+            return await utils.answer(m, self.strings("verify_status").format("already ON"))
         l.append(m.chat_id)
         self.db.set(self._db, "chats", l)
-        await utils.answer(m, self.strings("captcha_status").format("ON"))
+        await utils.answer(m, self.strings("verify_status").format("ON"))
+
+    async def chatcaptchaoffcmd(self, m: types.Message):
+        "Turn off captcha in chat"
+        l: list = self.db.get(self._db, "chats", [])
+        if m.chat_id not in l:
+            return await utils.answer(m, self.strings("verify_status").format("already OFF"))
+        l.remove(m.chat_id)
+        self.db.set(self._db, "chats", l)
+        await utils.answer(m, self.strings("verify_status").format("OFF"))
